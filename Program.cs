5649c0d61c1c49fcdab4b85b3d230b6017b3ec3d
gustavo.rocha <gustavorocha.net@hotmail.com>
2023-10-11 00:04:49 -0300

    Realizado alterações conforme a orientação do desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "Modelo 01", imei: "11111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Uber");
+
+System.Console.WriteLine();
+
+System.Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "345678", modelo: "Modelo 02", imei: "22222222", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Facebook");
+
+
